203536a00f62c9fc58fb9e50b193323299513e00
satish <satish2021choudhary@gmail.com>
2024-08-29 01:21:55 +0530

    create card view

---
+++ app/api/member/create/route.js
@@ -0,0 +1,26 @@
+import UserDetails from "@/model2/UserDetails";
+export const POST = async (request, { params }) => {
+  try {
+    const requestBody = await request.json();
+
+
+    const UserDetailsnew = new UserDetails({
+      name: requestBody?.name,
+      email: requestBody?.email,
+      gender: requestBody?.gender,
+      dob: requestBody?.dob,
+      relation: requestBody?.relation
+    });
+
+    await UserDetailsnew.save();
+
+
+    console.log("UserDetailsnew",UserDetailsnew)
+
+
+    return new Response(UserDetailsnew, { status: 200 });
+  } catch (error) {
+    console.log("error", error)
+    return new Response("Errorr", { status: 500 });
+  }
+};
--- model2/UserDetails.js
+++ model2/UserDetails.js
@@ -5,8 +5,8 @@ const { Schema } = mongoose;
 const UserDetailsSchema = new Schema({
   contactPhone: { type: String },
   name: { type: String, required: true },
-  email: { type: String, unique: true },//to be discuss unique ness
-  gender: { type: String, enum: ['male', 'female', 'other'] },
+  email: { type: String },//to be discuss unique ness
+  gender: { type: String},
   relation: { type: String },
   image: { type: String },
   dob: { type: Date },
@@ -14,5 +14,6 @@ const UserDetailsSchema = new Schema({
   deleted: { type: Boolean, default: false },
   is_delete: { type: Boolean, default: false },
 });
+
 
 export default mongoose.models.UserDetails || mongoose.model('UserDetails', UserDetailsSchema);
