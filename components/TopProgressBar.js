7466e109861f6a948b35a0cd346bcb0988ef501c
Anil Puri <anilgoswami@startbitsolutions.com>
2024-09-08 21:18:33 +0530

    Update TopProgressBar.js

--- components/TopProgressBar.js
+++ components/TopProgressBar.js
@@ -1,14 +1,9 @@
-"use client";
-
-import { useEffect, useState } from "react";
+"use client"
 import Router from "next/router";
 import NProgress from "nprogress";
 import "nprogress/nprogress.css";
-import "./TopProgressBar.css";
-
-// Configure NProgress
+import "./TopProgressBar.css"
 NProgress.configure({ showSpinner: false });
-
 let timer;
 let state;
 let activeRequests = 0;
@@ -23,7 +18,7 @@ function load() {
 
     timer = setTimeout(function () {
         NProgress.start();
-    }, delay); // Only show progress bar if it takes longer than the delay
+    }, delay); // only show progress bar if it takes longer than the delay
 }
 
 function stop() {
@@ -37,52 +32,34 @@ function stop() {
     NProgress.done();
 }
 
-export default function TopProgressBar() {
-    useEffect(() => {
-        // Set up route change events
-        Router.events.on("routeChangeStart", load);
-        Router.events.on("routeChangeComplete", stop);
-        Router.events.on("routeChangeError", stop);
+Router?.events?.on?.("routeChangeStart", load);
+Router?.events?.on?.("routeChangeComplete", stop);
+Router?.events?.on?.("routeChangeError", stop);
 
-        // Ensure the cleanup of route change events
-        return () => {
-            Router.events.off("routeChangeStart", load);
-            Router.events.off("routeChangeComplete", stop);
-            Router.events.off("routeChangeError", stop);
-        };
-    }, []);
+const originalFetch = window?.fetch;
 
-    useEffect(() => {
-        // Client-side fetch override
-        if (typeof window !== "undefined") {
-            const originalFetch = window?.fetch;
-
-            window.fetch = async function (...args) {
-                if (activeRequests === 0) {
-                    load();
-                }
-
-                activeRequests++;
-
-                try {
-                    const response = await originalFetch(...args);
-                    return response;
-                } catch (error) {
-                    return Promise.reject(error);
-                } finally {
-                    activeRequests -= 1;
-                    if (activeRequests === 0) {
-                        stop();
-                    }
-                }
-            };
-
-            // Cleanup fetch override on unmount
-            return () => {
-                window.fetch = originalFetch;
-            };
+if (window) {
+    window.fetch = async function (...args) {
+        if (activeRequests === 0) {
+            load();
         }
-    }, []);
 
+        activeRequests++;
+
+        try {
+            const response = await originalFetch(...args);
+            return response;
+        } catch (error) {
+            return Promise.reject(error);
+        } finally {
+            activeRequests -= 1;
+            if (activeRequests === 0) {
+                stop();
+            }
+        }
+    };
+}
+
+export default function () {
     return null;
 }
