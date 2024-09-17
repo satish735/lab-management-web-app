137794cf7d93abb9fa3b910aa089f87e2b6e03f1
Anil Puri <anil@ouverture.ai>
2024-09-17 15:04:05 +0530

    Update SideBarProfile.js

--- components/profile-side-bar/SideBarProfile.js
+++ components/profile-side-bar/SideBarProfile.js
@@ -1,76 +1,72 @@
 'use client'
 import React, { useState, useEffect } from 'react'
-import { FaLocationDot, FaRegAddressBook,FaUserGroup } from 'react-icons/fa6'
+import { FaLocationDot, FaRegAddressBook, FaUserGroup } from 'react-icons/fa6'
 import { MdAccountCircle } from 'react-icons/md'
-import {  useRouter } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 
 const SideBarProfile = () => {
     const router = useRouter();
-console.log("window.location.href", window.location.pathname)
-    const [active,setActive]=useState('MyBookings')
-
-    useEffect(()=> {
-
-        if(window.location.pathname == '/my-profile'){
+    const pathname = usePathname()
+    const [active, setActive] = useState('MyBookings')
+    useEffect(() => {
+        if (pathname == '/my-profile') {
             setActive('MyProfile')
-        }else if(window.location.pathname == '/mybookings'){
+        } else if (pathname == '/mybookings') {
             setActive('MyBookings')
-        }else if(window.location.pathname == '/my-address'){
+        } else if (pathname == '/my-address') {
             setActive('MyAddress')
-        }else if(window.location.pathname == '/myfamilymembers'){
+        } else if (pathname == '/myfamilymembers') {
             setActive('MyFamilyMembers')
         }
 
-    },[window?.location?.pathname])
+    }, [pathname])
     return (
         <div className=''  >
-
-
-            <div style={{cursor:'pointer'}} className={`my-4 ${active==='MyBookings' && 'is-active'}`} onClick={(()=>{
+            <div style={{ cursor: 'pointer' }} className={`my-4 ${active === 'MyBookings' && 'is-active'}`} onClick={(() => {
                 setActive('MyBookings')
                 router?.push('/mybookings')
-                })}>
+            })}>
 
-                <span className='mx-2'><FaRegAddressBook style={{fontSize:'20px'}} />
+                <span className='mx-2'><FaRegAddressBook style={{ fontSize: '20px' }} />
                 </span><span>My Bookings</span>
             </div>
 
 
 
-            <div style={{cursor:'pointer'}} className={`my-4 ${active==='MyAddress' && 'is-active'}`} onClick={(()=>{
+            <div style={{ cursor: 'pointer' }} className={`my-4 ${active === 'MyAddress' && 'is-active'}`} onClick={(() => {
                 setActive('MyAddress')
                 router?.push('/my-address')
 
-                })}>
+            })}>
 
                 <span >
 
-                <span className='mx-2'> <FaLocationDot style={{fontSize:'20px'}} />
-                </span><span>My Address</span></span>
+                    <span className='mx-2'> <FaLocationDot style={{ fontSize: '20px' }} />
+                    </span><span>My Address</span></span>
             </div>
 
 
 
 
-            <div style={{cursor:'pointer'}} className={`my-4 ${active==='MyProfile' && 'is-active'}`} 
-            onClick={(()=>{
-                setActive('MyProfile')
-                router?.push('/my-profile')
+            <div style={{ cursor: 'pointer' }} className={`my-4 ${active === 'MyProfile' && 'is-active'}`}
+                onClick={(() => {
+                    setActive('MyProfile')
+                    router?.push('/my-profile')
 
                 })}>
 
-                <span className='mx-2'><MdAccountCircle style={{fontSize:'20px'}} />
+                <span className='mx-2'><MdAccountCircle style={{ fontSize: '20px' }} />
                 </span><span>My Profile</span>
             </div>
 
-            <div style={{cursor:'pointer'}} className={`my-4 ${active==='MyFamilyMembers' && 'is-active'}`} 
-            onClick={(()=>{
-                setActive('MyFamilyMembers')
-                router?.push('/myfamilymembers')
+            <div style={{ cursor: 'pointer' }} className={`my-4 ${active === 'MyFamilyMembers' && 'is-active'}`}
+                onClick={(() => {
+                    setActive('MyFamilyMembers')
+                    router?.push('/myfamilymembers')
 
                 })}>
 
-                <span className='mx-2'><FaUserGroup style={{fontSize:'20px'}} />
+                <span className='mx-2'><FaUserGroup style={{ fontSize: '20px' }} />
                 </span><span>My Family Members</span>
             </div>
 
